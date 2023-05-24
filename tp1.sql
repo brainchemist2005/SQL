5aa410144acabcfad0b5caac45907b786a557fb6
brainchemist2005 <zackariaebouargan@gmail.com>
2023-05-24 02:04:21 -0400

    Question 2

--- tp1.sql
+++ tp1.sql
@@ -1,5 +1,12 @@
+/*1*/
 SELECT DISTINCT t1.id, t2.course_id
 FROM takes t1
 INNER JOIN teaches t2 ON t1.course_id = t2.course_id
                     AND t1.sec_id = t2.sec_id
 WHERE t2.course_id IN ('CS-101', 'CS-347', 'CS-315');
+
+
+/*2*/
+select t1.dept_name from department t1 , department t2
+where t1.budget > t2.budget and t2.dept_name = 'Music' 
+order by t1.dept_name desc;
